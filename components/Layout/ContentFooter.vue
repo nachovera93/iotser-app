d8a2b74087cc48133138a946c7f1f04218bc62d8
nachovera93 <ricardoignaciovera.93@gmail.com>
2021-08-10 16:43:49 -0400

    first commit app

---
+++ components/Layout/ContentFooter.vue
@@ -0,0 +1,59 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <ul class="nav">
+        <li class="nav-item">
+          <a
+            href="https://SerIOT.cl"
+            target="_blank"
+            rel="noopener"
+            class="nav-link"
+          >
+            SerIOT.cl
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            href="https://SerIOT.cl"
+            target="_blank"
+            rel="noopener"
+            class="nav-link"
+          >
+            Sobre Nosotros
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            href="hhttps://SerIOT.cl"
+            target="_blank"
+            rel="noopener"
+            class="nav-link"
+          >
+            Blog
+          </a>
+        </li>
+      </ul>
+      <div class="copyright">
+        &copy; {{ year }}, Hecho con <i class="tim-icons icon-heart-2"></i> por
+        
+        <a
+          href="https://IorSER.cl"
+          target="_blank"
+          rel="noopener"
+          >SER IOT</a
+        >
+        para una mejor web.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
---
+++ components/Widgets/Simple.vue
@@ -0,0 +1,105 @@
+<template>
+    <div>
+        <card>
+            <template slot="header">
+            <h5 class="card-category pull-right">{{getTimeAgo((nowTime - time) / 1000)}} ago </h5>
+
+            <h2 class="card-category">{{ config.selectedDevice.name }} - {{ config.variableFullName }}</h2>
+             
+            <h3 class="card-title">
+                <span>{{value.toFixed(config.decimalPlaces)}} {{config.unit}}</span>
+             </h3>
+
+        </template> 
+        </card>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['config'],
+    data(){
+        return{
+            receivedTime: 0,
+            value: 0,
+            time: Date.now(),
+            nowTime: Date.now(),
+            topic: ""
+        };
+    },
+    watch:  {
+            config: {
+                immediate: true,
+                deep: true,
+                handler() {
+                    setTimeout(() => {
+                        this.value = 0;
+                        this.$nuxt.$off(this.topic + "/sdata");
+                        this.topic = this.config.userId + '/' + this.config.selectedDevice.dId + '/' + this.config.variable;
+                        this.$nuxt.$on(this.topic + "/sdata", this.processReceivedData);                
+                    }, 300);
+                }
+            }
+        },
+
+      mounted() {
+
+            this.getNow();
+            //this.updateColorClass();
+
+        },
+        beforeDestroy() {
+            this.$nuxt.$off(this.topic + "/sdata");
+        },
+
+    methods:{
+
+    
+               processReceivedData(data){
+                try {
+                  this.time = Date.now();
+                  this.value = data.value;
+
+                } catch (error) {
+                  console.log(error);
+                }
+                  },
+
+            getNow() {
+                this.nowTime = Date.now();
+                setTimeout(() => {
+                    this.getNow();
+                }, 1000);
+            },
+
+            getTimeAgo(seconds) {
+
+                if (seconds < 0) {
+                    seconds = 0;
+                }
+
+                if (seconds < 59) {
+                    return seconds.toFixed() + " secs";
+                }
+
+                if (seconds > 59 && seconds <= 3600) {
+                    seconds = seconds / 60;
+                    return seconds.toFixed() + " min";
+                }
+
+                if (seconds > 3600 && seconds <= 86400) {
+                    seconds = seconds / 3600;
+                    return seconds.toFixed() + " hour";
+                }
+
+                if (seconds > 86400) {
+                    seconds = seconds / 86400;
+                    return seconds.toFixed() + " day";
+                }
+
+
+            },
+    }
+};
+</script>
